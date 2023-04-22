ff46ecd53ac3536ec42572eba597b92412c5402e
Siddharth Gupta <siddharth11tha@gmail.com>
2023-04-22 19:23:20 +0530

    Create All Indices of Number.cpp

---
+++ recursion-1/All Indices of Number.cpp
@@ -0,0 +1,18 @@
+
+int allIndexes(int input[], int size, int x, int output[]) {
+
+  if (size == 0) {
+    return 0;
+  }
+
+  int ans = allIndexes(input, size - 1, x, output);
+  
+  if( input[size-1] == x)
+  {   
+     output[ans] = size-1;
+     ans = ans +1;
+   }
+
+  return ans;
+
+}
