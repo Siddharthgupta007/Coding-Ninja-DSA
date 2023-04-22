b7003fa6e4055e064eada9e6e84e913358ede29f
Siddharth Gupta <siddharth11tha@gmail.com>
2023-04-22 19:53:15 +0530

    Create Swap two Nodes of LL.cpp

---
+++ Linked LIst 2/Swap two Nodes of LL.cpp
@@ -0,0 +1,89 @@
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node *swapNodes(Node *head, int i, int j)
+{
+	//Write your code here
+	if(i>j){
+        i=i+j;
+        j=i-j;
+        i=i-j;
+    }
+    //Find nodes
+	//case 1
+    if(i==0){
+		// case 2
+        if(j==1){
+            Node *cur=head->next;
+            head->next=cur->next;
+            cur->next=head;
+            return cur;
+        }
+		//case 2.1
+        Node *pt=head;
+        for(int c=1;c<j;c++)
+            pt=pt->next;
+        
+        Node *temp=head->next;
+        Node *cur=pt->next;
+        
+        head->next=cur->next;
+        pt->next=head;
+        cur->next=temp;
+        return cur;
+        
+    }
+
+	//case 3
+    if(j-i==1){
+        Node * pt=head;
+        for(int c=1;c<i;c++)
+            pt=pt->next;
+        
+        Node* cur=pt->next;
+        Node* cur1=cur->next;
+        
+        cur->next=cur1->next;
+        pt->next=cur1;
+        cur1->next=cur;
+        return head;
+        
+    }
+
+	//genral case
+    Node* pt1=head;
+    Node *pt2=head;
+    
+    for(int c=1;c<i;c++)
+        pt1=pt1->next;
+    
+    for(int c=1;c<j;c++)
+        pt2=pt2->next;
+    
+    Node *cur1=pt1->next;
+    Node *cur2=pt2->next;
+    
+    Node *temp=cur1->next;
+    
+    cur1->next=cur2->next;
+    pt2->next=cur1;
+    pt1->next=cur2;
+    cur2->next=temp;
+    
+    return head;
+}
