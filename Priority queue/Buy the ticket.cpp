92f438a940c63ba6c9db0e17075a9deb42cd12d8
Siddharth Gupta <siddharth11tha@gmail.com>
2023-04-23 10:42:31 +0530

    Create Buy the ticket.cpp

---
+++ Priority queue/Buy the ticket.cpp
@@ -0,0 +1,35 @@
+#include<queue>
+#include<vector>
+
+int buyTicket(int *arr, int n, int k) {
+    // Write your code here
+
+    queue<int> p; //a queue of indices
+    for(int i=0;i<n ;i++)
+         p.push(i);
+
+          priority_queue <int > pq;
+
+          for(int i=0 ; i<n ;i++)
+    {
+        pq.push(arr[i]);
+    }
+
+    int time=0;
+
+while( p.front()!=k || pq.top()!= arr[p.front()] ) 
+{   
+    if(pq.top()>arr[p.front()])
+    { int x=p.front();
+        p.pop();
+       p.push(x);
+    }
+    
+    else if(pq.top()==arr[p.front()])
+    { pq.pop();
+      p.pop();
+       time++;		
+    }
+}
+    return time+1; 
+}
