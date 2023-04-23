55dece9588c35cbe24c5ece0e741263c00b73973
Siddharth Gupta <siddharth11tha@gmail.com>
2023-04-23 10:21:41 +0530

    Create Find height.cpp

---
+++ Trees/Find height.cpp
@@ -0,0 +1,42 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+int getHeight(TreeNode<int>* root) {
+    // Write your code here
+    if(root==NULL)
+    {
+        return 0;
+    }
+    int ans = 0;
+    
+    for(int i=0; i<root->children.size(); i++)
+    {
+       int childheight=getHeight(root->children[i]);
+       if(childheight>ans){
+           ans=childheight;
+       }
+    }
+
+    return ans+1;
+        
+}
