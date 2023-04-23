8ac69d91a84697755b719cdebd7dd7e6f9914ba1
Siddharth Gupta <siddharth11tha@gmail.com>
2023-04-23 10:28:31 +0530

    Create Construct Tree from Postorder and Inorder.cpp

---
+++ Binary Tree/Construct Tree from Postorder and Inorder.cpp
@@ -0,0 +1,60 @@
+/***********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+int search(int *inorder, int start, int end, int curr)
+{
+	for(int i=start; i<=end; i++)
+	{
+		if(inorder[i] == curr)
+		return i;
+	}
+
+	return -1;
+}
+BinaryTreeNode<int> *help(int *preorder, int *inorder, int start, int end)
+{ 
+	static int idx =end;
+
+
+	if(start>end)
+	return NULL;
+	
+	int curr = preorder[idx];
+	idx--;
+	
+	BinaryTreeNode<int> *Temp = new BinaryTreeNode<int>(curr);
+
+
+	if(start == end)
+	return Temp;
+
+
+	int pos = search(inorder, start, end, curr);
+	Temp->right = help(preorder, inorder, pos+1, end);
+
+	Temp->left=help(preorder, inorder, start, pos-1);
+	
+
+	return Temp;
+
+}
+
+BinaryTreeNode<int>* buildTree(int *postorder, int postLength, int *inorder, int inLength) {
+    // Write your code here
+	return help(postorder,inorder, 0, inLength-1);
+}
